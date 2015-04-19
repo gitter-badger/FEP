5655d57840b0c2b3eaf559f6201a194c945a8122
YetAnotherMinion <yetanotherminion@gmail.com>
2015-04-18 20:09:58 -0400

    migrating regions works

--- src/a3.cc
+++ src/a3.cc
@@ -22,22 +22,78 @@ int main(int argc, char** argv)
   apf::MeshEntity* e;
   apf::Adjacent up;
 
+  apf::Numbering* vert_nums = apf::numberOwnedDimension(m, "verts", 0);
+  apf::Numbering* reg_nums = apf::numberOwnedDimension(m, "regions", 3);
+
   if(PCU_Comm_Self() == 0) {
     std::cout << "number of peers" << PCU_Comm_Peers() << std::endl;
     it = m->begin(0);
     apf::Copies remotes;
     while(e = m->iterate(it)) {
       m->getRemotes(e, remotes);
-      for(apf::Copies::iterator it = remotes.begin(); it != remotes.end(); ++it) {
-	std::cout << "shared with:" << it->second << std::endl; 
+      if(m->isOwned(e)) {
+	for(apf::Copies::iterator it = remotes.begin(); it != remotes.end(); ++it) {
+	  std::cout << e << " shared with:" << it->second << std::endl; 
+	}
+      }
+      std::cout << "====loop====" << std::endl;
+    }
+  }
+  //Part 3
+  apf::writeVtkFiles("pre_migr", m);
+  //migration plan should exist for each process
+  apf::Migration* plan = new apf::Migration(m);
+  
+  //get the mesh regions that are on boundary
+  it = m->begin(3);
+  //count the number of faces on the partition model face
+  int partition_face_cnt = 0;
+  while(e = m->iterate(it)) {
+    //we assume that there are no shadow regions
+    //otherwise the adjacencies would not make much sense
+    apf::Downward faces;
+    int num_faces = m->getDownward(e, 2, faces);
+    int migrate_flag = 0;
+    int destination = -1;
+    for(int ii = 0; ii < num_faces; ++ii) {
+      if( ! m->isOwned(faces[ii])) {
+	++partition_face_cnt;
+	if(PCU_Comm_Self() == 0) { 
+	  if(! plan->has(e)) {
+	    plan->send(e, m->getOwner(faces[ii]));
+	  }
+	}
       }
     }
   }
-  
-  apf::Numbering* vert_nums = apf::numberOwnedDimension(m, "verts", 0);
-  
+
+  printf("Partition %d reports %d partition faces before migration\n", \
+	 PCU_Comm_Self() , partition_face_cnt);
+  //now send all of the migration plan
+  m->migrate(plan);
+
+  //now compute the number of shared faces
+  it = m->begin(3);
+  //count the number of faces on the partition model face
+  int aftr_partition_face_cnt = 0;
+  while(e = m->iterate(it)) {
+    //we assume that there are no shadow regions
+    //otherwise the adjacencies would not make much sense
+    apf::Downward faces;
+    int num_faces = m->getDownward(e, 2, faces);
+    int migrate_flag = 0;
+    int destination = -1;
+    for(int ii = 0; ii < num_faces; ++ii) {
+      if( ! m->isOwned(faces[ii])) {
+	++aftr_partition_face_cnt;
+      }
+    }
+  }
+  printf("Partition %d reports %d partition faces after migration\n", \
+	 PCU_Comm_Self() , aftr_partition_face_cnt);
+
  
-  apf::writeVtkFiles("part",m); 
+  apf::writeVtkFiles("post_migr", m); 
   m->destroyNative();
   apf::destroyMesh(m);
   PCU_Comm_Free();
