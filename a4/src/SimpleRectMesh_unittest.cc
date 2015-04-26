3cc1b1d3429da662440f7a5d7594faceb45d1ea4
YetAnotherMinion <yetanotherminion@gmail.com>
2015-04-26 15:16:13 -0400

    found number of nodes on face

--- a4/src/SimpleRectMesh_unittest.cc
+++ a4/src/SimpleRectMesh_unittest.cc
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdio.h>
 
 #include <gtest/gtest.h>
 
@@ -6,6 +7,7 @@
 #include <apfMesh2.h>
 #include <apfMDS.h>
 #include <apfShape.h>
+#include <apfField.h>
 
 #include "MeshBuilder.h"
 
@@ -34,9 +36,34 @@ protected:
 
 TEST_F(RectMeshTest, Rectangle) {
 	mesh_builder->build2DRectQuadMesh(mesh, 2, 1, 0, 0, 2, 1);
-	apf::writeVtkFiles("outQuad", mesh);
-	apf::changeMeshShape(mesh, apf::getSerendipity());
-	apf::writeVtkFiles("secondQuad", mesh);
+	//apf::writeVtkFiles("outQuad", mesh);
+	//apf::changeMeshShape(mesh, apf::getSerendipity());
+	apf::changeMeshShape(mesh, apf::getLagrange(1));
+	//apf::writeVtkFiles("secondQuad", mesh);
+	apf::MeshEntity* e;
+	apf::MeshIterator* it = mesh->begin(2);
+	//apf::Field * master_f = createFieldOn(mesh, "foo", apf::VECTOR); 
+	apf::Field* master_f = createFieldOn(mesh, "foo", apf::SCALAR);
+	apf::FieldShape* fs = mesh->getShape();
+
+	while((e = mesh->iterate(it))) {
+		//apf::MeshElement* mesh_elm = apf::getMeshEle
+		//apf::Element* element_ptr = apf::createElement(master_f, e );
+		apf::Vector3 param;
+		double xyz[3] = {0, 0 , 0};
+		param.fromArray(xyz);
+		int order = 2;
+		int rule = order * order;
+		// for(int ii = 0; ii < rule; ++ ii) {
+		// 	apf::getGaussPoint(, order, ii, param);
+		// 	//std::cout << param << std::endl;
+		// }
+		std::cout << apf::countElementNodes(fs, mesh->getType(e)) << std::endl;
+		//std::cout << master_f->countComponents() << std::endl;
+		apf::EntityShape* es = fs->getEntityShape(mesh->getType(e));
+		std::cout << "Order: " <<  fs->getOrder() << std::endl;
+		std::cout << fs->countNodesOn(apf::Mesh::EDGE) << std::endl;
+	}
 }
 
 TEST_F(RectMeshTest, Triangle) {
