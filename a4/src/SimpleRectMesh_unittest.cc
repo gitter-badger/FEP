7e0879ee7e94434387975bd7c3b14de84d0f3311
YetAnotherMinion <yetanotherminion@gmail.com>
2015-04-27 21:50:40 -0400

    figured out shape function mapping

--- a4/src/SimpleRectMesh_unittest.cc
+++ a4/src/SimpleRectMesh_unittest.cc
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <iomanip>
 
 #include <gtest/gtest.h>
 
@@ -8,6 +9,7 @@
 #include <apfMDS.h>
 #include <apfShape.h>
 #include <apfField.h>
+#include <apfNumbering.h>
 
 #include "MeshBuilder.h"
 
@@ -37,32 +39,59 @@ protected:
 TEST_F(RectMeshTest, Rectangle) {
 	mesh_builder->build2DRectQuadMesh(mesh, 2, 1, 0, 0, 2, 1);
 	//apf::writeVtkFiles("outQuad", mesh);
-	//apf::changeMeshShape(mesh, apf::getSerendipity());
-	apf::changeMeshShape(mesh, apf::getLagrange(1));
+	apf::changeMeshShape(mesh, apf::getSerendipity());
+	//apf::changeMeshShape(mesh, apf::getLagrange(1));
 	//apf::writeVtkFiles("secondQuad", mesh);
 	apf::MeshEntity* e;
 	apf::MeshIterator* it = mesh->begin(2);
 	//apf::Field * master_f = createFieldOn(mesh, "foo", apf::VECTOR); 
 	apf::Field* master_f = createFieldOn(mesh, "foo", apf::SCALAR);
 	apf::FieldShape* fs = mesh->getShape();
+	apf::Numbering* nodes_numbers = apf::numberOwnedNodes(mesh, "fatman", fs);
 
 	while((e = mesh->iterate(it))) {
-		//apf::MeshElement* mesh_elm = apf::getMeshEle
+		//apf::MeshElement* mesh_elm = apf::getMeshElement()
 		//apf::Element* element_ptr = apf::createElement(master_f, e );
+		//apf::EntityShape::getValues
 		apf::Vector3 param;
-		double xyz[3] = {0, 0 , 0};
-		param.fromArray(xyz);
-		int order = 2;
-		int rule = order * order;
-		// for(int ii = 0; ii < rule; ++ ii) {
-		// 	apf::getGaussPoint(, order, ii, param);
-		// 	//std::cout << param << std::endl;
-		// }
+		double zeros[3] = {0, 0, 0};
+
+		param.fromArray(zeros);
+		int order = 4; //this is the order accuracy, not the polynomial degree
+		apf::MeshElement* mesh_elm = apf::createMeshElement(mesh, e);
+		int num_int_points = apf::countIntPoints(mesh_elm, order);
+		apf::EntityShape* es = fs->getEntityShape(mesh->getType(e));
+		uint32_t num_nodes = apf::countElementNodes(fs, mesh->getType(e));
+
+		apf::NewArray< int > node_mapping(num_nodes);
+
+		apf::getElementNumbers(nodes_numbers, e, node_mapping);
+
+		for(int ii = 0; ii < num_nodes; ++ii) {
+			std::cout << ii << " => " << node_mapping[ii] << std::endl;
+		}
+
+		for(int ii = 0; ii < num_int_points; ++ ii) {
+			double running_sum = 0;
+
+			apf::getIntPoint(mesh_elm, order, ii, param);
+			std::cout << std::setprecision(16);
+			std::cout << ii << ": " << param.x() << " " << param.y() << std::endl;
+
+			apf::NewArray< apf::Vector3 > grads(num_nodes);
+			es->getLocalGradients(mesh, e, param, grads);
+
+			for(uint32_t jj = 0; jj < num_nodes; ++jj ) {
+				std::cout << grads[jj].x() << " " << grads[jj].y() << " " << grads[jj].z() << std::endl;
+				//apf::Vector3 xi(3,3,3);
+				//std::cout << ii << ": " << xi.x() << ", " << xi.y() << ", " << xi.z() << std::endl;
+				//fs->getNodeXi(apf::Mesh::QUAD, ii, xi);
+				//std::cout << ii << ": " << xi.x() << ", " << xi.y() << ", " << xi.z() << std::endl;
+			}
+			std::cout << "========" << ii << "========" << std::endl;
+		}
 		std::cout << apf::countElementNodes(fs, mesh->getType(e)) << std::endl;
 		//std::cout << master_f->countComponents() << std::endl;
-		apf::EntityShape* es = fs->getEntityShape(mesh->getType(e));
-		std::cout << "Order: " <<  fs->getOrder() << std::endl;
-		std::cout << fs->countNodesOn(apf::Mesh::EDGE) << std::endl;
 	}
 }
 
