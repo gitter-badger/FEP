7882afc66fe976af2cde434faa38417aab9e800f
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-10 22:14:21 -0400

    start reading using expat

---
+++ a4/src/parseVtk.cc
@@ -0,0 +1,16 @@
+#include <expat.h>
+
+#include "apf.h"
+#include "apfNumbering.h"
+
+#include "apfVtk.h"
+
+
+
+static void readPvtuFile(const char* filename, Mesh* m) {
+
+}
+
+static void readVtuFile(const char* filename, apf::Numbering* n) {
+
+}
