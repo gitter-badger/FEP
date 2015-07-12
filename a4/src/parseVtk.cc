ec4a6a6f9c210f1d912b26df10c87454fe6b37c3
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-11 21:31:45 -0400

    found inlcude location from public api

--- a4/src/parseVtk.cc
+++ a4/src/parseVtk.cc
@@ -1,14 +1,22 @@
+#include <fstream>
+#include <sstream>
+
 #include <expat.h>
 
-#include "apf.h"
-#include "apfNumbering.h"
+#include <apf.h>
+/*apf.h includes the definition of the writeVtkFiles,
+note that most of the helper functions called by
+writeVtkFiles are static, which is no good to us,
+maybe we will ask for some reuse? */
+#include <apfMesh.h>
+#include <apfNumbering.h>
+#include <apfNumberingClass.h>
+#include <apfShape.h>
+#include <apfFieldData.h>
 
-#include "apfVtk.h"
 
-
-
-static void readPvtuFile(const char* filename, Mesh* m) {
-
+static void readPvtuFile(const char* filename, apf::Mesh* m) {
+	apf::writeVtkFiles("foo", m);
 }
 
 static void readVtuFile(const char* filename, apf::Numbering* n) {
