ba6ea2aa0940baf546592abb22637647d92159c3
YetAnotherMinion <yetanotherminion@gmail.com>
2015-07-23 22:16:20 -0400

    now reads attributes names

--- a4/inc/vtkSAX2Handler.h
+++ a4/inc/vtkSAX2Handler.h
@@ -1,4 +1,5 @@
 #include <xercesc/sax2/DefaultHandler.hpp>
+#include <xercesc/sax2/Attributes.hpp>
 
 using namespace xercesc_3_1;
 
@@ -12,5 +13,12 @@ public:
         const   XMLCh* const    qname,
         const   Attributes&     attrs
     );
+
+   	void endElement(
+   		const 	XMLCh* const uri,
+   		const 	XMLCh* const localname,
+   		const 	XMLCh* const qname
+   	);
+
     void fatalError(const SAXParseException&);
 };
--- a4/src/vtkSAX2Handler.cc
+++ a4/src/vtkSAX2Handler.cc
@@ -17,6 +17,25 @@ void vtkSAX2Handler::startElement(const   XMLCh* const    uri,
     char* message = XMLString::transcode(localname);
     cout << "I saw element: "<< message << endl;
     XMLString::release(&message);
+    XMLSize_t len, ii;
+    len = attrs.getLength();
+    std::cout << "  number of attrs: = " << len << std::endl;
+    const XMLCh* attr_name;
+    for(ii = 0; ii < len; ++ii){
+        attr_name = attrs.getQName(ii);
+        message = XMLString::transcode(attr_name);
+        std::cout << "attr: " << message << std::endl;
+        XMLString::release(&message); 
+    }
+}
+
+void vtkSAX2Handler::endElement(const   XMLCh* const uri,
+        const   XMLCh* const localname,
+        const   XMLCh* const qname)
+{
+    char* message = XMLString::transcode(localname);
+    std::cout << "\tend of element: " << message << std::endl;
+    XMLString::release(&message);
 }
 
 void vtkSAX2Handler::fatalError(const SAXParseException& exception)
@@ -27,3 +46,4 @@ void vtkSAX2Handler::fatalError(const SAXParseException& exception)
          << endl;
     XMLString::release(&message);
 }
+
