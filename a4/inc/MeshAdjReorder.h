af3e4739aabbce77de7a87505b7e388e2056b45d
YetAnotherMinion <yetanotherminion@gmail.com>
2015-08-13 13:25:22 -0400

    fixed comments, removed printing, extended labeling for arbirary dimensions

--- a4/inc/MeshAdjReorder.h
+++ a4/inc/MeshAdjReorder.h
@@ -4,7 +4,8 @@
 #include <apfNumbering.h>
 #include <apfMesh.h>
 #include <apfShape.h>
+#include <stdint.h>
 
-void adjReorder(apf::Mesh* m, apf::FieldShape* shape, int components,
+void adjReorder(apf::Mesh* m, apf::FieldShape* shape, uint32_t components,
             apf::Numbering* & inNodes, apf::Numbering* & elmNodes);
 #endif
--- a4/src/MeshAdjReorder.cc
+++ a4/src/MeshAdjReorder.cc
@@ -21,8 +21,12 @@ bool hasNode(apf::Mesh* m, apf::MeshEntity* e)
 }
 
 void getGoodStartingElement(apf::Mesh* m, apf::MeshEntity* & start, int dim) {
-    // unsigned int min_order = 400;
     apf::MeshIterator* it = m->begin(dim);
+    /*below removed for cases where geometric model does not
+    * have any vertices, because the below depends on that invariant
+    * so we disable it until later when our code guarentees it*/
+    #if 0
+    // unsigned int min_order = 400;
     // while((e = m->iterate(it))){
     //     apf::ModelEntity* model_ent = m->toModel(e);
     //     assert(model_ent != NULL);
@@ -40,10 +44,10 @@ void getGoodStartingElement(apf::Mesh* m, apf::MeshEntity* & start, int dim) {
     //         start= e; 
     //     }
     // }
-
-    //return the first vertex
+    #endif
+    /* return the first vertex*/
     start = m->iterate(it);
-    std::cout << start << std::endl;
+    // std::cout << start << std::endl;
 }
 
 struct sortVertsFunctor {
@@ -60,31 +64,31 @@ struct sortVertsFunctor {
 
 void adjReorder(apf::Mesh* m, 
             apf::FieldShape* shape, 
-            int components,
+            uint32_t n_components,
             apf::Numbering* & nodeNums,
             apf::Numbering* & faceNums) 
 {
     apf::MeshEntity* e;
     apf::MeshIterator* it;
-    int node_label = 0;
+    signed long node_label = 0;
     int dim = m->getDimension();
 
     for(int ii = 0; ii <= dim; ++ii){
         if(shape->hasNodesIn(ii)){
             it = m->begin(ii);
             while((e = m->iterate(it))) {
-                std::cout << e << std::endl;
+                // std::cout << e << std::endl;
                 if(m->isOwned(e)) {
-                    node_label += shape->countNodesOn(m->getType(e)) * components;
+                    node_label += shape->countNodesOn(m->getType(e)) * n_components;
                 }
             }
         }
     }
 
-    // deduct any fixed entities from the locally-owned total
+    /* deduct any fixed entities from the locally-owned total */
     apf::MeshEntity* start_vert = NULL;
     getGoodStartingElement(m, start_vert, 0);
-    std::cout << start_vert << std::endl;
+    // std::cout << start_vert << std::endl;
     std::queue<apf::MeshEntity*> q;
     std::set<apf::MeshEntity*> s;
     q.push(start_vert);
@@ -92,10 +96,14 @@ void adjReorder(apf::Mesh* m,
     std::set<apf::MeshEntity*>::iterator set_it;
     std::vector<apf::MeshEntity*> ent_list;
 
-    signed int face_label = m->count(2);
-    //we stop at zero because we labeled every node
-    //everything left in the queue will already have been labeled
-    while( q.size() > 0 && node_label != 0) {
+    /*switch to zero based indexing*/
+    node_label -= 1;
+    signed int face_label = m->count(2) - 1;
+    /*
+    * we stop at zero because we labeled every node
+    * everything left in the queue will already have been labeled
+    */
+    while( q.size() > 0 && node_label >= 0) {
         apf::MeshEntity* current_entity;
         current_entity = q.front();
         q.pop();
@@ -103,17 +111,21 @@ void adjReorder(apf::Mesh* m,
         if(set_it != s.end()) {
             s.erase(set_it);
         }
-        //assuming that there is only one dof per entity
+        /*assuming that there is only one dof per entity*/
         if(!apf::isNumbered(nodeNums, current_entity,0,0)) {
-            apf::number(nodeNums, current_entity, 0, 0, node_label);
-            --node_label;
+            for(uint32_t kk = 0; kk < n_components; ++kk){
+                apf::number(nodeNums, current_entity, 0, kk, node_label);
+                --node_label;
+            }
         } else {
             continue;
         }
 
-        //now we want to add any unlabled adjacent mesh entities to the queue
-        //additions to queue will only be handled  by looking at adjacencies from
-        //vertices
+        /*
+        * now we want to add any unlabled adjacent mesh entities to the queue
+        * additions to queue will only be handled  by looking at adjacencies from
+        * vertices
+        */
         if(apf::getDimension(m, current_entity) == 0) {
             apf::MeshEntity* vert = current_entity;
             apf::Adjacent edge_adj;
@@ -137,24 +149,26 @@ void adjReorder(apf::Mesh* m,
                     //std::cout << "trying to queue a face node" << std::endl;
                     }
                 }
-                //get the other end of the edge
+                /*get the other end of the edge */
                 apf::MeshEntity* other_vert = apf::getEdgeVertOppositeVert(m, curr_edge, vert);
                 if(hasNode(m, curr_edge)) {
-                    //check that the other_vert is labeled or in queue and edge node not labeled
+                    /*check that the other_vert is labeled or in queue and edge node not labeled*/
                     set_it = s.find(other_vert);
                     bool is_s = (set_it != s.end());
                     if( (apf::isNumbered(nodeNums, other_vert, 0, 0) || is_s ) &&
                         !apf::isNumbered(nodeNums, curr_edge, 0, 0) ) {
-                    apf::number(nodeNums, curr_edge, 0 , 0, node_label);
-                    --node_label;
+                        for(uint32_t kk = 0; kk < n_components; ++kk) {
+                            apf::number(nodeNums, curr_edge, 0 , kk, node_label);
+                            --node_label;
+                        }
                     } else {
-                        //check that this edge is not labeled or in queue
+                        /*check that this edge is not labeled or in queue*/
                         set_it = s.find(curr_edge);
                         if(!apf::isNumbered(nodeNums, curr_edge, 0, 0) && set_it == s.end()) {
                             q.push(curr_edge);
                             s.insert(curr_edge);
                         }
-                        //add the other vertex to the list
+                        /*add the other vertex to the list*/
                         ent_list.push_back(other_vert);
                     }
                 } else {
@@ -162,17 +176,16 @@ void adjReorder(apf::Mesh* m,
                         ent_list.push_back(other_vert);
                     }
                 }
-            }// finished loop over all edges coming into vertex
-            //add all of the elements in the list to the queue
+            }/* finished loop over all edges coming into vertex */
+            /* add all of the elements in the list to the queue */
             if(ent_list.size() > 0) {
-                //use the functor to create a closure over the mesh for ranking the vector
+                /*use the functor to create a closure over the mesh for ranking the vector */
                 struct sortVertsFunctor functor;
                 functor.m = m;
                 std::sort(ent_list.begin(), ent_list.end(), functor);
-                //add each element of the vector to the queue
-                //print out the ent vector and the queue
+                /*add each element of the vector to the queue*/
                 for(std::vector<apf::MeshEntity*>::iterator it = ent_list.begin(); it != ent_list.end(); ++it) {
-                    //check that it is not already in queue
+                    /*check that entity is not already in queue*/
                     set_it = s.find(*it);
                     if(set_it == s.end()) {
                         q.push(*it);
@@ -185,15 +198,17 @@ void adjReorder(apf::Mesh* m,
             }
         } else {
             // std::cout << "not a vertex" << std::endl;
-        } // end of vertex checking
+        } /* end of vertex checking*/
         if(q.size() > 500 ) {
             // std::cout << "bad termination" << std::endl;
             break;
         }
-    } //end of while loop
-    //make sure that we did not leave an unlabeled node in the queue
+    } /*end of while loop */
+    /*make sure that we did not leave an unlabeled node in the queue */
     while(!q.empty()) {
-        assert(apf::isNumbered(nodeNums, q.front(), 0, 0) == 1);
+        for(uint32_t kk = 0; kk < n_components; ++kk){
+            assert(apf::isNumbered(nodeNums, q.front(), 0, kk) == 1);
+        }
         q.pop();
     }
 }
