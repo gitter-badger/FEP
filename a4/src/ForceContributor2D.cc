7c62e7e1384f59b02381bbde6672ef978d080bf4
YetAnotherMinion <yetanotherminion@gmail.com>
2015-08-14 16:46:50 -0400

    adding force contributor

---
+++ a4/src/ForceContributor2D.cc
@@ -0,0 +1,47 @@
+#include <apfMesh.h>
+
+#include "ForceContributor2D.h"
+
+ForceContributor2D::ForceContributor2D(
+    apf::Field* f,
+    uint32_t integrate_order,
+    apf::Vector3(*fnc)(apf::Vector3 const& p)) : apf::Integrator(integrate_order), field(f)
+{
+    this->ndims = apf::getMesh(f)->getDimension();
+    this->fnc  =fnc;
+}
+
+void ForceContributor2D::inElement(apf::MeshElement* me)
+{
+    /*create the Field Element from the MeshElement*/
+    this->field_element = apf::createElement(this->field, me);
+    /*determine the size of the force matrices*/
+    this->nnodes = apf::countNodes(this->field_element);
+    this->ndofs = this->ndims * apf::countNodes(this->field_element);
+    this->fe.setSize(this->ndofs);
+    /*zero the internal matricies*/
+    this->fe.zero();
+}
+
+void ForceContributor2D::outElement()
+{
+    /*perform clean up and destroy specific field element*/
+    apf::destroyElement(this->field_element);
+}
+
+void ForceContributor2D::atPoint(apf::Vector3 const& p, double w, double dV)
+{
+    apf::NewArray<double> shape_val;
+    apf::getShapeValues(this->field_element, p, shape_val);
+
+    apf::Vector3 x;
+    apf::MeshElement* me = apf::getMeshElement(this->field_element);
+    apf::mapLocalToGlobal(me, p, x);
+    /*compute the contributions of each shape function*/
+    apf::Vector3 tmp = this->fnc(x);
+    for(uint32_t ii = 0; ii < this->nnodes; ++ii){
+        for(uint32_t jj = 0; jj < this->ndims; ++jj){
+            this->fe += (shape_val[ii] * tmp[jj] * w * dV);
+        }
+    }
+}
