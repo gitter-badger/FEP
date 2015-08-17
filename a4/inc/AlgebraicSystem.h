1f932cd7a66926ad2533536f7115c8b02de99f58
YetAnotherMinion <yetanotherminion@gmail.com>
2015-08-17 11:35:59 -0400

    assembly process working

--- a4/inc/AlgebraicSystem.h
+++ a4/inc/AlgebraicSystem.h
@@ -11,31 +11,41 @@
 
 #include "BandedSymmetricMatrix.h"
 
+#define KNOWN_DOF_MASK ((uint64_t)1<<63)
+
 class AlgebraicSystem
 {
 public:
 	AlgebraicSystem(std::size_t global_n_dofs);
 	~AlgebraicSystem();
 
+	void addBoundaryConstraint(
+		std::vector<double> fixed,
+		apf::NewArray<int> const& node_mapping,
+		uint32_t size);
+
+	void beginAssembly();
+
 	void assemble(
 		apf::DynamicMatrix const& ke,
 		apf::NewArray<int> const& node_mapping,
-		uint32_t nLocalDOFs);
+		uint32_t size);
 	void assemble(
 		std::vector<double> const& fe,
 		apf::NewArray<int> const& node_mapping,
-		uint32_t nLocalDOFs);
+		uint32_t size);
 
 private:
+	bool _allow_assembly;
+	std::map< std::size_t,uint64_t > masks;
+	std::vector< double > known_d;
+
 	Mat K;
     Vec d;
     Vec F;
     KSP solver;
 
 	std::size_t ndofs;
-	/*we keep track of which degrees of freedom are
-	* fixed by boundary constraints first*/
-	std::map<std::size_t, uint64_t> mask;
 };
 
 #endif
--- a4/src/AlgebraicSystem.cc
+++ a4/src/AlgebraicSystem.cc
@@ -1,23 +1,92 @@
 #include <stdexcept>
+#include <climits>
 
 #include "AlgebraicSystem.h"
 
 AlgebraicSystem::AlgebraicSystem(std::size_t global_n_dofs) : ndofs(global_n_dofs)
 {
-
+	this->_allow_assembly = false;
+	/*preallocate the map since its keys will run from [0, ndofs-1]*/
+	for(std::size_t ii = 0; ii < this->ndofs; ++ii) {
+		/* -1 is valid intializer for unsigned types since this puts us at
+		* UINT_MAX for what ever type we are using. Using this value as 
+		* sentinal value for unitialized is valid, since this number
+		* represents the indice on a square matrix. Since PETSc,
+		* which will be handling this matrix, has never done more than
+		* 500 million rows in a matrix before on super computers, it is
+		* reasonable to assume that we will never be in danger of overflow
+		* for this value for the next 20 years*/
+		this->masks.insert(std::map< size_t, uint64_t >::value_type(ii, -1));
+	}
 }
 
 AlgebraicSystem::~AlgebraicSystem()
 {
+	/*only destroy the matricies if they were allocated*/
+	if(true == this->_allow_assembly) {
+		MatDestroy(&(this->K));
+		VecDestroy(&(this->d));
+		VecDestroy(&(this->F));
+		KSPDestroy(&(this->solver));
+	}
+}
+
+void AlgebraicSystem::beginAssembly() {
+	_allow_assembly = true;
+
+
+	/*now compute the remaining degrees of freedom*/
+	std::size_t ndogs = this->known_d.size();
+	std::size_t n_eq = this->ndofs - ndogs;
+	/*we construct the stiffness matrix only for degress of freedom
+	* which have not been fixed, 
+	* WARNING: this program is intended to run on a single process
+	* there for the local size of all petsc variables is set to
+	* the same as the global size.*/
+	VecCreateMPI(PETSC_COMM_WORLD, n_eq, n_eq, &(this->F));
+	std::cout << "here" << std::endl;
+	VecSetOption(this->F, VEC_IGNORE_NEGATIVE_INDICES, PETSC_TRUE);
+		std::cout << "here" << std::endl;
+
+	MatCreateAIJ(PETSC_COMM_WORLD, n_eq, n_eq, n_eq, n_eq,
+		300, PETSC_NULL, 300, PETSC_NULL, &(this->K));
+		std::cout << "here" << std::endl;
+
+	MatSetOption(this->K, MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
+		std::cout << "here" << std::endl;
+
+	KSPCreate(PETSC_COMM_WORLD, &(this->solver));
+		std::cout << "here" << std::endl;
+
+	KSPSetTolerances(this->solver, 1.0e-8, 1.0e-8, PETSC_DEFAULT, 100);
+		std::cout << "here" << std::endl;
+
+	VecDuplicate(this->F, &(this->d));
+		std::cout << "here" << std::endl;
+}
+
+void AlgebraicSystem::addBoundaryConstraint(
+	std::vector<double> fixed,
+	apf::NewArray<int> const& node_mapping,
+	uint32_t size)
+{
+	/*do not add a constraint after assembly has begun*/
+	if(this->_allow_assembly == true) {
+		throw std::invalid_argument("cannot add constraint after assembly has started");
+	}
 
 }
 
-
 void AlgebraicSystem::assemble(
-		apf::DynamicMatrix const& ke,
-		apf::NewArray<int> const& node_mapping,
-		uint32_t nLocalDOFs)
+	apf::DynamicMatrix const& ke,
+	apf::NewArray<int> const& node_mapping,
+	uint32_t nLocalDOFs)
 {
+	/*CHECK if assembly is possible*/
+	if(this->_allow_assembly == false) {
+		throw std::invalid_argument("must call beginAssembly before assembly");
+	}
+
 	/*iterate over the input matrix and add each value
 	* into the global matrix according to the mapping in */
 	std::size_t nrows = ke.getRows();
